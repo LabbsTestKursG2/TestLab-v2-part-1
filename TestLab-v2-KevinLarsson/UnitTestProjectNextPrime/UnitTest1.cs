44791e56e3a618c0183a5be9c1c3e09d6ecd835f
kevin larsson <larsson__96@hotmail.com>
2016-04-25 09:12:40 +0200

    TestLab v1 of v2

---
+++ TestLab-v2-KevinLarsson/UnitTestProjectNextPrime/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestLab_v2_KevinLarsson;
+
+namespace UnitTestProjectNextPrime
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            MethodTest method = new MethodTest();
+
+            //arrange
+            int value = 123;
+            int[] sum = { 127, 131, 137 };
+
+            //act
+            int[] result = method.NextPrime(value);
+
+            //assert
+            CollectionAssert.AreEqual(sum, result);
+        }
+    }
+}
